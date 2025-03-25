1b4d30897874a1c7b7c0d05dc7d79be94286a775
Saumya Shah <saumyabshah90@gmail.com>
2025-03-25 10:22:26 +0530

    func to broadcast trancs
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- core/network.go
+++ core/network.go
@@ -1,12 +1,29 @@
 package core
 
-import "sync"
-
-var (
-	peerNodes   = []string{"http://node1:8080", "http://node2:8080"}
-	networkLock = sync.Mutex{}
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"sync"
 )
 
-func BroadcastTransactions(tx LicenseTransaction) {
+var (
+	peerNodes    = []string{"http://node1:8080", "http://node2:8080"}
+	networkMutex = sync.Mutex{}
+)
 
+// There can be many transaction in a block?? currently there is just one transac which might go into a block
+// TODO: Check how/why there can be multiple of them
+func BroadcastTransactions(tx LicenseTransaction) {
+	networkMutex.Lock()
+	defer networkMutex.Unlock()
+
+	marshalContent, err := json.Marshal(tx)
+	if err != nil {
+		panic(err) // TODO: add loging
+	}
+
+	for _, node := range peerNodes {
+		http.Post(node+"/receive_transaction", "application/json", bytes.NewBuffer(marshalContent))
+	}
 }
